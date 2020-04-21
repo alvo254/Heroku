4a85987a5eda0fe5dc4ffe9f878cefe955379e2b
Nelson Gacheru <nelsonzachariah@gmail.com>
2020-04-21 22:44:33 +0300

    attempts to change the nav bar

--- uhuru/static/js/app.js
+++ uhuru/static/js/app.js
@@ -3,15 +3,22 @@ const navSlide = () => {
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li');
     //Toggle Nav
-    burger.addEventListener('click',() => {
+    burger.addEventListener('onClick',() => {
         nav.classList.toggle('nav-active');
+        //Animate Links
+        navLinks.forEach((link,index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`;
+            }
+        });
+        //Burger Animation
+        burger.classList.toggle('toggle');
+
     });
-    //Animate Links
-    navLinks.forEach((link,index) => {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`;
-    })
+
 }
 
-
-    navSlide();
+navSlide();
 
